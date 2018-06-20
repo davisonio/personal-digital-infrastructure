9855554971b2c2cdcd088ce887ebb78bb839946e
Craig Davison <craig@davison.io>
2018-06-20 01:04:48 +0100

    Big improvements
    - ssh, user and ufw setup
    - minetest setup
    - secrets and vars
     -group vars

--- main.auto.tfvars
+++ main.auto.tfvars
@@ -1,2 +1,3 @@
 # Generate using: ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}'
 ssh_fingerprint = "f2:cc:a8:57:c2:14:a1:7d:9d:a1:14:39:75:18:d0:93"
+ssh_name = "craig@craig-macbook"
--- main.tf
+++ main.tf
@@ -5,85 +5,40 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
-resource "digitalocean_tag" "nginx2" {
-    name = "nginx2"
+resource "digitalocean_ssh_key" "default" {
+  name       = "${var.do_token}"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
-resource "digitalocean_droplet" "nginx_server" {
-    name = "nginx-server"
+resource "digitalocean_tag" "minetest" {
+    name = "minetest"
+}
+
+resource "ansible_group" "minetest" {
+  inventory_group_name = "minetest"
+}
+
+resource "digitalocean_droplet" "minetest1" {
+    name = "minetest1.davison.io"
     image = "ubuntu-18-04-x64"
     size = "512mb"
     region = "lon1"
     ipv6 = true
-    private_networking = false
+    private_networking = true
     monitoring = true
     tags = [
-        "${digitalocean_tag.nginx2.id}"
+        "${digitalocean_tag.minetest.id}"
     ]
     ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
-}
-
-resource "digitalocean_ssh_key" "default" {
-  name       = "craig@craig-macbook"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
-
-resource "digitalocean_domain" "examplecom" {
-    name = "example289.com"
-    ip_address = "${digitalocean_droplet.nginx_server.ipv4_address}"
-}
-
-resource "digitalocean_record" "examplecom" {
-    name = "examplecom"
-    type = "A"
-    domain = "${digitalocean_domain.examplecom.name}"
-    value = "${digitalocean_droplet.nginx_server.ipv4_address}"
-}
-
-resource "digitalocean_firewall" "webserver" {
-    name = "webserver-firewall"
-    droplet_ids = ["${digitalocean_droplet.nginx_server.id}"]
-
-    inbound_rule = [
-      {
-        protocol = "tcp"
-        port_range = "22"
-      },
-      {
-        protocol = "tcp"
-        port_range = "80"
-      },
-      {
-        protocol = "tcp"
-        port_range = "443"
-      }
-    ]
-
-    outbound_rule = [
-      {
-        protocol = "tcp"
-        port_range = "53"
-      },
-      {
-        protocol = "udp"
-        port_range = "53"
-      }
-    ]
-}
-
-resource "ansible_host" "example" {
-    inventory_hostname = "example289.com"
-    groups = ["web"]
-    vars {
-        ansible_user = "admin"
+    #volume_ids = ["${digitalocean_volume.minetest1-world.id}"]
+    provisioner "remote-exec" {
+        inline = [
+            "apt install -y python",
+        ]
     }
 }
 
-resource "ansible_group" "web" {
-  inventory_group_name = "web"
-  children = ["foo", "bar", "baz"]
-  vars {
-    foo = "bar"
-    bar = 2
-  }
+resource "ansible_host" "minetest1" {
+    inventory_hostname = "minetest1.davison.io"
+    groups = ["minetest"]
 }
