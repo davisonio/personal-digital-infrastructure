33977ee80b5514526a01f10d00da8ef3945f1726
Craig Davison <craig@davison.io>
2018-09-03 22:35:38 +0100

    Add eurodiscover droplet infra

--- main.tf
+++ main.tf
@@ -18,6 +18,14 @@ resource "ansible_group" "minetest" {
   inventory_group_name = "minetest"
 }
 
+resource "digitalocean_tag" "discourse" {
+    name = "discourse"
+}
+
+resource "ansible_group" "discourse" {
+  inventory_group_name = "discourse"
+}
+
 resource "digitalocean_droplet" "minetest1" {
     name = "minetest1.davison.io"
     image = "ubuntu-18-04-x64"
@@ -47,4 +55,34 @@ resource "digitalocean_droplet" "minetest1" {
 resource "ansible_host" "minetest1" {
     inventory_hostname = "minetest1.davison.io"
     groups = ["minetest"]
+}
+
+resource "digitalocean_droplet" "eurodiscover" {
+    name = "eurodiscover.davison.io"
+    image = "ubuntu-18-04-x64"
+    size = "512mb"
+    region = "lon1"
+    ipv6 = true
+    private_networking = true
+    monitoring = true
+    tags = [
+        "${digitalocean_tag.discourse.id}"
+    ]
+    ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
+    connection {
+        type     = "ssh"
+        user     = "root"
+        private_key = "${file("~/.ssh/id_rsa")}"
+    }
+    provisioner "remote-exec" {
+        inline = [
+            "sleep 30",
+            "apt install -y python",
+        ]
+    }
+}
+
+resource "ansible_host" "eurodiscover" {
+    inventory_hostname = "eurodiscover.davison.io"
+    groups = ["discourse"]
 }
